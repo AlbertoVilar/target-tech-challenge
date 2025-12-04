32348657427e7150e00a3771d03ab708f488a4da
Alberto Vilar <albertovilar1@gmail.com>
2025-12-04 20:16:24 -0300

    feat: add SalesWrapper to support JSON root structure

---
+++ src/main/java/com/target/challenge/model/SalesWrapper.java
@@ -0,0 +1,15 @@
+package com.target.challenge.model;
+
+import java.util.List;
+
+public class SalesWrapper {
+
+    private List<Sale> vendas;
+
+    public List<Sale> getVendas() {
+        return vendas;
+    }
+    public void setVendas(List<Sale> vendas) {
+        this.vendas = vendas;
+    }
+}
